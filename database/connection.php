c5a59992c0a83647d8088e8562141ab9003f4a8d
rekqN <pmreisf@gmail.com>
2024-01-30 19:40:18 +0000

    ajustes na landing page

--- database/connection.php
+++ database/connection.php
@@ -4,13 +4,13 @@ try {
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
     $DATABASE_PASS = '';
-    $DATABASE_NAME = 'bd-sir';
+    $DATABASE_NAME = 'bd_sir';
 
     $pdo = new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    echo "Cannot connect to database. Try again later";
+    echo "Ups! Cannot connect do db 😭";
     echo $e->getMessage();
     file_put_contents('PDOErrors.txt', $e->getMessage(), FILE_APPEND);
     exit();
--- pages/public/signin.php
+++ pages/public/signin.php
@@ -8,13 +8,13 @@ require_once __DIR__ . '/../../middleware/middleware-not-authenticated.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SpendWise - Sign In</title>
-
+    <link rel="icon" href="../../landingPage/assets/images/icon-1.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../pageResources/styles/global.css">
   </head>
     
-  <body style="background-image: url('../../landingPage/assets/images/hero-1.jpg')" class="py-5">
+  <body style="background-image: url('../../landingPage/assets/images/hero-3.jpg')" class="py-5">
     <section class="py-4 px-5">
       <?php
         if (isset($_SESSION['errors'])) {
@@ -34,31 +34,23 @@ require_once __DIR__ . '/../../middleware/middleware-not-authenticated.php';
           <div class="col-lg-6 mb-5 mb-lg-0">
             <div class="d-flex justify-content-center">
               <a class="text-decoration-none" href="../../index.php">
-                <h1 class="fw-bold" style="color: blueviolet">EXPENSE FLOW</h1>
+                <h1 class="fw-bold" style="color: blueviolet">SPEND WISE</h1>
               </a>
             </div>
-            <p style="color: hsl(217, 10%, 50.8%)">
-              Hello there! Returning to your financial hub? Sign in to your account and unlock a world of personalized financial management. Our platform is more than just numbers; it's about your journey, your story, and your financial well-being.
-            </p>
           </div>
 
           <div class="col-lg-6 mb-5 mb-lg-0">
             <div class="card">
               <div class="card-body py-5 px-md-5">
                 <form action="../../controllers/auth/signin.php" method="post">
-                  <!-- Email input -->
                   <div class="form-outline mb-4">
                     <label class="mb-2" for="Email">Email Adress</label>
                     <input type="email" class="form-control" id="Email" placeholder="Email" name="email" maxlength="255" value="<?= isset($_REQUEST['email']) ? $_REQUEST['email'] : null ?>">
                   </div>
-
-                  <!-- Password input -->
                   <div class="form-outline mb-4">
                     <label class="mb-2" for="password">Password</label>
                     <input type="password" class="form-control" id="password" placeholder="Password" name="password" maxlength="255" value="<?= isset($_REQUEST['password']) ? $_REQUEST['password'] : null ?>">
                   </div>
-
-                  <!-- CheckBox -->
                   <div class="form-check d-flex justify-content-start mb-4">
                     <div class="justify-content-start">
                       <input class="form-check-input" type="checkbox" id="remember-me">
