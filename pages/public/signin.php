95cabf3bd4a0afc03f859e694f2aa3875681113e
rekqN <pmreisf@gmail.com>
2024-01-30 02:51:14 +0000

    sign in em processo

--- pages/public/signin.php
+++ pages/public/signin.php
@@ -1,41 +1,91 @@
 <?php
-require_once __DIR__ . '/../../infra/middleware/middleware-not-authenticated.php';
-include_once __DIR__ . '../../../templates/header.php';
+require_once __DIR__ . '/../../middleware/middleware-not-authenticated.php';
+?>
 
-$title = ' - Sign In';
-?>
-<main>
-  <section>
-    <?php
-    if (isset($_SESSION['errors'])) {
-      echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">';
-      foreach ($_SESSION['errors'] as $error) {
-        echo $error . '<br>';
-      }
-      echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
-      unset($_SESSION['errors']);
-    }
-    ?>
-  </section>
-  <form action="/crud/controllers/auth/signin.php" method="post">
-    <h1 class="h3 mb-3 fw-normal">Sign In</h1>
-    <div class="form-floating mb-2">
-      <input type="email" class="form-control" id="Email" placeholder="Email" name="email" maxlength="255"
-        value="<?= isset($_REQUEST['email']) ? $_REQUEST['email'] : null ?>">
-      <label for="Email">Email</label>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SpendWise - Sign In</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../pageResources/styles/global.css">
+  </head>
+    
+  <body style="background-image: url('../../landingPage/assets/images/hero-1.jpg')" class="py-5">
+    <section class="py-4 px-5">
+      <?php
+        if (isset($_SESSION['errors'])) {
+          echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">';
+          foreach ($_SESSION['errors'] as $error) {
+            echo $error . '<br>';
+          }
+          echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+          unset($_SESSION['errors']);
+        }
+      ?>
+    </section>
+
+    <div class="px-4 py-5 px-md-5 text-center text-lg-start">
+      <div class="container">
+        <div class="row gx-lg-5 align-items-center">
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <div class="d-flex justify-content-center">
+              <a class="text-decoration-none" href="../../index.php">
+                <h1 class="fw-bold" style="color: blueviolet">EXPENSE FLOW</h1>
+              </a>
+            </div>
+            <p style="color: hsl(217, 10%, 50.8%)">
+              Hello there! Returning to your financial hub? Sign in to your account and unlock a world of personalized financial management. Our platform is more than just numbers; it's about your journey, your story, and your financial well-being.
+            </p>
+          </div>
+
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <div class="card">
+              <div class="card-body py-5 px-md-5">
+                <form action="../../controllers/auth/signin.php" method="post">
+                  <!-- Email input -->
+                  <div class="form-outline mb-4">
+                    <label class="mb-2" for="Email">Email Adress</label>
+                    <input type="email" class="form-control" id="Email" placeholder="Email" name="email" maxlength="255" value="<?= isset($_REQUEST['email']) ? $_REQUEST['email'] : null ?>">
+                  </div>
+
+                  <!-- Password input -->
+                  <div class="form-outline mb-4">
+                    <label class="mb-2" for="password">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Password" name="password" maxlength="255" value="<?= isset($_REQUEST['password']) ? $_REQUEST['password'] : null ?>">
+                  </div>
+
+                  <!-- CheckBox -->
+                  <div class="form-check d-flex justify-content-start mb-4">
+                    <div class="justify-content-start">
+                      <input class="form-check-input" type="checkbox" id="remember-me">
+                    </div>
+                    <div class="justify-content-end">
+                      <label class="form-check-label" for="remember-me">
+                        Remember me
+                      </label>
+                    </div>          
+                  </div>
+
+                  <!-- Submit button -->
+                  <div class="d-flex justify-content-center">
+                    <button class="w-50 btn btn-lg btn-blueviolet mb-4" type="submit" name="user" value="login">Sign In</button>
+                  </div>
+                  <div class="d-flex justify-content-center mb-2">
+                    Create an account 
+                    <a href="./signup.php" class="text-decoration-none mx-1" style="color:blueviolet;">
+                        here
+                    </a>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="form-floating mb-2">
-      <input type="password" class="form-control" id="password" placeholder="Password" name="password" maxlength="255"
-        value="<?= isset($_REQUEST['password']) ? $_REQUEST['password'] : null ?>">
-      <label for="password">Password</label>
-    </div>
-    <div class="checkbox mb-3">
-      <label><input type="checkbox" value="remember-me">Remember me</label>
-    </div>
-    <button class="w-100 btn btn-lg btn-success mb-2" type="submit" name="user" value="login">Sign In</button>
-  </form>
-  <a href="/crud"><button class="w-100 btn btn-lg btn-info">Back</button></a>
-</main>
-<?php
-include_once __DIR__ . '../../../templates/footer.php';
-?>
+  </body>
+</html>
